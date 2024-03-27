26e6fa8acc29ec438b46507df1d458a52a1d39f2
oarthurfc <arthuryouface@gmail.com>
2024-03-27 14:10:48 -0300

    Inicializando projeto Spring #7 e #31

---
+++ Codigo/back-end/depinhomultimidias/src/main/java/com/depinhomultimidias/depinhomultimidias/DepinhomultimidiasApplication.java
@@ -0,0 +1,13 @@
+package com.depinhomultimidias.depinhomultimidias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepinhomultimidiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepinhomultimidiasApplication.class, args);
+	}
+
+}
---
+++ Codigo/back-end/depinhomultimidias/src/test/java/com/depinhomultimidias/depinhomultimidias/DepinhomultimidiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.depinhomultimidias.depinhomultimidias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepinhomultimidiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
