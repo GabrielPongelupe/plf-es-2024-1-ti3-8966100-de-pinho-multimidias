81396203cfde61c418248eacd468472c0b615832
GabrielPongelupe <gabriel.carvalho.1446145@sga.pucminas.br>
2024-05-05 13:27:30 -0300

    update integração  #46

--- Codigo/front-end/src/js/cadastroProduto.js
+++ Codigo/front-end/src/js/cadastroProduto.js
@@ -1,45 +1,65 @@
 // variaveis do cadastro
-var formulario = document.getElementById("forms-cadastro-produto");
-var nome = document.getElementById("nomeProduto").value;
-var tipoProduto = document.getElementById("tipoProduto").value;
-var preco = parseFloat(document.getElementById("precoProduto").value);
-var comandoVolante = document.getElementById("checkbox1").checked;
-var radioOriginal = document.getElementById("checkbox2").checked;
-var anoInicio = parseInt(document.getElementById("anoInicio").value);
-var anoFinal = parseInt(document.getElementById("anoFinal").value);
-var videoRelacionado = document.getElementById("videoRelacionado").value;
 var botao = document.getElementById("btn-cadastrar-produto");
-var descricao = document.getElementById("descricaoGrande").value;
 
-// url 
-var urlCadastroProduto = "http://127.0.0.1:8080/produto";
+var formulario = document.getElementById("forms-cadastro-produto");
+formulario.addEventListener('submit', function (event) {
+    // Impede o envio do formulário para que possamos processar os valores antes
+    event.preventDefault();
 
-// token da localstorage
-var token = localStorage.getItem("token");
+    var nome = document.getElementById("nomeProduto").value;
+    var tipoProduto = document.getElementById("tipoProduto").value;
+    var preco = parseFloat(document.getElementById("precoProduto").value);
+    var comandoVolante = document.getElementById("checkbox1").checked;
+    var radioOriginal = document.getElementById("checkbox2").checked;
+    var anoInicio = parseInt(document.getElementById("anoInicio").value);
+    var anoFinal = parseInt(document.getElementById("anoFinal").value);
+    var videoRelacionado = document.getElementById("videoRelacionado").value;
+    var descricao = document.getElementById("descricaoGrande").value;
+
+    // url 
+    var urlCadastroProduto = "http://127.0.0.1:8080/produto";
+
+    // token da localstorage
+    var token = localStorage.getItem("token");
 
 
-// funcao para cadastrar produto
-async function cadastrarProduto() {
-    try {
-        const response = await axios.post(urlCadastroProduto, {
-            nome: nome,
-            preco: preco,
-            descricao: descricao,
-            anoInicio: anoInicio,
-            anoFim: anoFinal,
-            videoRelacionado: videoRelacionado,
-            tipoProduto: tipoProduto,
-            possuiComandoVolante: comandoVolante,
-            possuiRadioOriginal: radioOriginal
-        }, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-                "Content-Type": "application/json" 
-            }
+    // funcao para cadastrar produto
+    async function cadastrarProduto() {
+        console.log(nome, tipoProduto, preco, comandoVolante, radioOriginal, anoInicio, anoFinal, videoRelacionado, descricao)
+
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        myHeaders.append("Authorization", `Bearer ${token}`);
+
+        const raw = JSON.stringify({
+            "nome": nome,
+            "preco": preco,
+            "descricao": descricao,
+            "anoInicio": anoInicio,
+            "anoFim": anoFinal,
+            "videoRelacionado": videoRelacionado,
+            "tipoProduto": tipoProduto,
+            "possuiComandoVolante": comandoVolante,
+            "possuiRadioOriginal": radioOriginal
         });
-        console.log(response.data);
-        return response.data;
-    } catch (error) {
-        console.log("erro ao cadastrar produto");
+
+        const requestOptions = {
+            method: "POST",
+            headers: myHeaders,
+            body: raw,
+            redirect: "follow"
+        };
+
+        fetch(urlCadastroProduto, requestOptions)
+            .then((response) => response.text())
+            .then((result) => console.log(result))
+            .catch((error) => console.error(error));
     }
-}
+
+
+botao.addEventListener("click", function (e) {
+    e.preventDefault();
+    cadastrarProduto();
+});
+})
+
