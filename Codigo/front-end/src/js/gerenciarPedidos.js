7932998b0f6baaee070e1e9f66beb60789210cfd
renatoctti <renato.cazzoletti7@gmail.com>
2024-06-04 18:11:42 -0300

    java script

--- Codigo/front-end/src/js/gerenciarPedidos.js
+++ Codigo/front-end/src/js/gerenciarPedidos.js
@@ -4,17 +4,18 @@ document.addEventListener("DOMContentLoaded", () => {
   const editStatusBtn = document.querySelectorAll(".edit-status-btn");
   const modalEditarRastreio = document.getElementById("modal-editar-codigo-rastreio");
   const modalEditarStatus = document.getElementById("modal-editar-status");
+  const closestatusBtn = document.getElementById("btn-modal-close-status");
   const closeRastreioBtn = document.getElementById("btn-modal-close-rastreio");
-  const concluirEdicaoBtns = document.querySelectorAll(".concluir-edicao");
+  const concluirEdicaoStatus = document.getElementById("concluir-edicao-status");
+  const concluirEdicaoRastreio = document.getElementById("concluir-edicao-rastreio");
 
   const urlPedidos = "http://127.0.0.1:8080/pedido";
   const urlDadosCompra = "http://127.0.0.1:8080/dados-pedido/{id}";
   const urlItensPedido = "http://127.0.0.1:8080/item-pedido/{id}";
 
   // Função para mostrar todos os produtos na tela
-  
   async function getPedidos() {
-    //Paginação para dividir a visualização de produtos 
+    // Paginação para dividir a visualização de produtos 
     const params = {
         page: 0,
         size: 20
@@ -26,109 +27,82 @@ document.addEventListener("DOMContentLoaded", () => {
       console.log(pedidos);
 
       pedidos.forEach(pedido => {
-        container.innerHTML += `
-        <div class="col-md-6">
-        <div class="pedido p-5 mt-4">
-            <!--Dados do pedido-->
-            <div class="d-flex flex-row justify-content-between pb-4">
-              <ul class="p-0 m-0">
-                <li class="tituloLista">
-                  ID do pedido: <span id="idPedido" class="fw-normal">${pedido.id}</span>
-                </li>
-                <li class="tituloLista">
-                  Data do pedido:
-                  <span id="dataPedido" class="fw-normal">${pedido.momento}</span>
-                </li>
+        let produtosHtml = '';
 
-                <!-- ALTERAR PRA FAZER UM MODAL E ADICIONAR O DROPDOWN DENTRO DESSE MODAL -->
-                <li>
-                  <label for="tipoProduto">Status</label>
-                  <select class="form-control" id="status">
+        pedido.itens.forEach(item => {
+          produtosHtml += `
+            <div class="d-flex flex-row align-items-center produto mb-5">
+              <img src="${item.produto.imagem}" alt="" />
+              <div class="dados-produto">
+                <ul class="p-0 m-0">
+                  <li>Nome do produto: ${item.produto.nome}</li>
+                  <li>
+                    Código de rastreio:
+                    <span class="codigoRastreio fw-bold">${item.codigoRastreio}</span> //DEVE SER TIRADO O CODIGO DE RASTREIO
+                    <i class="bi bi-pencil-square edit-rastreio-btn"></i>
+                  </li>
+                  <li class="fw-bold">R$${item.produto.preco.toFixed(2)}</li>
+                </ul>
+              </div>
+            </div>
+          `;
+        });
+
+        container.innerHTML += `
+          <div class="col-md-6">
+            <div class="pedido p-5 mt-4">
+              <!-- Dados do pedido -->
+              <div class="d-flex flex-row justify-content-between pb-4">
+                <ul class="p-0 m-0">
+                  <li class="tituloLista">
+                    ID do pedido: <span id="idPedido" class="fw-normal">${pedido.id}</span>
+                  </li>
+                  <li class="tituloLista">
+                    Data do pedido:
+                    <span id="dataPedido" class="fw-normal">${pedido.momento}</span>
+                  </li>
+                  <li>
+                    <label for="tipoProduto">Status</label>
+                    <select class="form-control" id="status">
                       <option value="0">Aguardando Pagamento</option>
                       <option value="1">Pago</option>
                       <option value="2">Enviado</option>
                       <option value="3">Cancelado</option>
-                  </select> 
-              </li>
-              </ul>
-  
-              <ul class="p-0 me-5">
-                <li class="tituloLista">Contato</li>
-                <li id="nome">${pedido.dadosPedido.primeiroNome}</li>
-                <li id="cpf">${pedido.dadosPedido.cpf}</li>
-                <li id="telefone">${pedido.dadosPedido.telefone}</li>
-                <li id="email">${pedido.dadosPedido.email}</li>
-              </ul>
-            </div>
-  
-            <hr class="w-100" />
-  
-            <!--Dados de entrega-->
-            <div class="d-flex flex-row align-items-center py-4 dados-pessoais">
-  
-              <ul class="p-0 me-6">
-                <li class="tituloLista">Endereço de entrega</li>
-                <li id="endereco">${pedido.dadosPedido.rua}, ${pedido.dadosPedido.numero} - ${pedido.dadosPedido.bairro}</li>
-                <li id="cidadeEstado">${pedido.dadosPedido.cidade} - ${pedido.dadosPedido.estado}</li>
-                <li id="cep">${pedido.dadosPedido.cep}</li>
-                <li id="complemento">${pedido.dadosPedido.complemento}</li>
-              </ul>
-            </div>
-  
-            <hr class="w-100" />
-  
-            <!--Produtos-->
-            <div class="pt-4 produtos">
-
-              <div class="d-flex flex-row align-items-center produto mb-5">
-                <img
-                  <!-- ALTERAR PRA FAZER UM FOR EACH NA LISTA ITENS -->
-                  src="${pedido.itens.produto.imagem}"
-                  alt=""
-                />
-  
-                <div class="dados-produto">
-                  <ul class="p-0 m-0">
-                    <li>Nome do produto</li>
-                    <li>
-                      Código de rastreio:
-                      <span class="codigoRastreio fw-bold">XYZ244</span>
-                      <i class="bi bi-pencil-square edit-rastreio-btn"></i>
-                    </li>
-                    <li class="fw-bold">R$994,00</li>
-                  </ul>
-                </div>
+                    </select>
+                  </li>
+                </ul>
+                <ul class="p-0 me-5">
+                  <li class="tituloLista">Contato</li>
+                  <li id="nome">${pedido.dados_pedido.primeiroNome}</li>
+                  <li id="cpf">${pedido.dados_pedido.cpf}</li>
+                  <li id="telefone">${pedido.dados_pedido.telefone}</li>
+                  <li id="email">${pedido.dados_pedido.email}</li>
+                </ul>
               </div>
-  
-              <div class="d-flex flex-row align-items-center me-5 produto">
-                <img
-                  src="https://images.tcdn.com.br/img/img_prod/1197109/central_multimidia_navpro_quadcore_plus_11_1_0c1c487df3f7268223a07bf59365bc18.jpg"
-                  alt=""
-                />
-  
-                <div class="dados-produto">
-                  <ul class="p-0 m-0">
-                    <li>Nome do produto</li>
-                    <li>
-                      Código de rastreio:
-                      <span class="codigoRastreio fw-bold">XYZ244</span>
-                      <i class="bi bi-pencil-square edit-rastreio-btn"></i>
-                    </li>
-                    <li class="fw-bold">R$994,00</li>
-                  </ul>
-                </div>
+              <hr class="w-100" />
+              <!-- Dados de entrega -->
+              <div class="d-flex flex-row align-items-center py-4 dados-pessoais">
+                <ul class="p-0 me-6">
+                  <li class="tituloLista">Endereço de entrega</li>
+                  <li id="endereco">${pedido.dados_pedido.rua}, ${pedido.dados_pedido.numero} - ${pedido.dados_pedido.bairro}</li>
+                  <li id="cidadeEstado">${pedido.dados_pedido.cidade} - ${pedido.dados_pedido.estado}</li>
+                  <li id="cep">${pedido.dados_pedido.cep}</li>
+                  <li id="complemento">${pedido.dados_pedido.complemento}</li>
+                </ul>
               </div>
+              <hr class="w-100" />
+              <!-- Produtos -->
+              <div class="pt-4 produtos">
+                ${produtosHtml}
+              </div>
+              <p class="mt-5 fw-bold">TOTAL DO PEDIDO: R$${pedido.total.toFixed(2)}</p>
             </div>
-            <p class="mt-5 fw-bold">TOTAL DO PEDIDO: R$99999</p>
-        </div>
-    </div>
+          </div>
         `;
-    });
-
+      });
     } catch (error) {
       console.error('Erro ao obter produtos:', error);
     }
-
   }
 
   // Função para abrir modal de editar código de rastreio
@@ -164,15 +138,19 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 
-  //editRastreioBtn.forEach.addEventListener('click', openRastreioModal);
-
   // Event listeners para fechar modais
+  closestatusBtn.addEventListener("click", closeStatusModal);
   closeRastreioBtn.addEventListener("click", closeRastreioModal);
 
   // Event listeners para botões de salvar (fechar modal)
-  concluirEdicaoBtns.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      closeRastreioModal();
-    });
+  concluirEdicaoStatus.addEventListener("click", () => {
+    closeStatusModal();
   });
+
+  concluirEdicaoRastreio.addEventListener("click", () => {
+    closeRastreioModal();
+  });
+
+  // Chamada inicial para obter pedidos
+  getPedidos();
 });
