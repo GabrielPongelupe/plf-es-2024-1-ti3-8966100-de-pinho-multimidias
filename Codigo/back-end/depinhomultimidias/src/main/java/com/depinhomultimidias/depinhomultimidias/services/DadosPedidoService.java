761dc7a115dfa7e0b96d05d4541332a7cbc0e830
oarthurfc <arthuryouface@gmail.com>
2024-06-12 16:25:57 -0300

    fix: Removendo TipoPagamento do back-end

--- Codigo/back-end/depinhomultimidias/src/main/java/com/depinhomultimidias/depinhomultimidias/services/DadosPedidoService.java
+++ Codigo/back-end/depinhomultimidias/src/main/java/com/depinhomultimidias/depinhomultimidias/services/DadosPedidoService.java
@@ -54,10 +54,7 @@ public class DadosPedidoService {
         }
         if(dadosPedido.getCep() != null){
             newDadosPedido.setCep(dadosPedido.getCep());
-        }/*
-        if(dadosPedido.getFormaPagamento()!= null){
-            newDadosPedido.setFormaPagamento(dadosPedido.getFormaPagamento());
-        } */
+        }
         if (dadosPedido.getPedido() != null) {
             newDadosPedido.setPedido(dadosPedido.getPedido());
         }
